9c791f650a784d670565e53d207fa0e4740c57d6
Danichini <daniel.reverol@gmail.com>
2020-05-18 22:25:42 -0500

    build version updated to V1

--- navigation/defaultNav.js
+++ navigation/defaultNav.js
@@ -1,12 +1,24 @@
-import * as React from "react";
+import React, { useEffect } from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SQLite from "expo-sqlite";
+
 import SignUp from "../views/signUp";
 import Login from "../views/logIn";
 
 const Stack = createStackNavigator();
+const db = SQLite.openDatabase("db.db");
 
 export default function MyStack() {
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, email text, password text);"
+      );
+    }, null);
+    
+  }, [])
+
   return (
     <Stack.Navigator initialRouteName="SignUp">
       <Stack.Screen
